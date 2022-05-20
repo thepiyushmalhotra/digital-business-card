71a1976557e0b29c1a5492dc7ec8ae79ff6cfec0
Piyush Malhotra <piyush@soulber.co>
2022-05-20 14:00:11 -0400

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey[900],
+        backgroundColor: Colors.grey[800],
         body: SafeArea(
             child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               radius: 50.0,
@@ -25,7 +27,7 @@ class MyApp extends StatelessWidget {
                 fontSize: 60.0,
                 fontFamily: 'Smooch',
                 fontWeight: FontWeight.bold,
-                color: Colors.grey[400],
+                color: Colors.grey[300],
               ),
             ),
             Text(
@@ -33,124 +35,92 @@ class MyApp extends StatelessWidget {
               style: TextStyle(
                 fontSize: 20.0,
                 letterSpacing: 2.5,
-                color: Colors.grey[400],
+                color: Colors.grey[300],
                 fontWeight: FontWeight.bold,
                 fontFamily: 'Source Sans Pro',
               ),
             ),
             SizedBox(
-              height: 5.0,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                color: Colors.grey[400],
-              ),
-              padding: EdgeInsets.all(10.0),
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: const <Widget>[
-                  Icon(
-                    Icons.code_rounded,
-                    color: Colors.black,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'I love to write code 👨‍💻',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.grey[600],
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                color: Colors.grey[400],
-              ),
-              padding: EdgeInsets.all(10.0),
+            Card(
+              color: Colors.grey[300],
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: const <Widget>[
-                  Icon(
-                    Icons.gamepad_rounded,
+              child: ListTile(
+                leading: Icon(
+                  Icons.code_rounded,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  'I love to write code 👨‍💻',
+                  style: TextStyle(
                     color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'And I love gaming 🎮',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                color: Colors.grey[400],
-              ),
-              padding: EdgeInsets.all(10.0),
+            Card(
+              color: Colors.grey[300],
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: const <Widget>[
-                  Icon(
-                    Icons.phone_android_outlined,
+              child: ListTile(
+                leading: Icon(
+                  Icons.gamepad_rounded,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  'And I love gaming 🎮',
+                  style: TextStyle(
                     color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+91 123 456 789',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                color: Colors.grey[400],
-              ),
-              padding: EdgeInsets.all(10.0),
+            Card(
+              color: Colors.grey[300],
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: const <Widget>[
-                  Icon(
-                    Icons.email_rounded,
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone_android_rounded,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  '+91 123 456 789',
+                  style: TextStyle(
                     color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
                   ),
-                  SizedBox(
-                    width: 10.0,
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.grey[300],
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email_rounded,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  'piyush.malhotra@outlook.com',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
                   ),
-                  Text(
-                    'piyush.malhotra@outlook.com',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
